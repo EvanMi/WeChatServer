b992409cab111d8a3bfef2282ddf96e20bdf8409
mipengcheng <mipengcheng3@jd.com>
2024-09-07 18:09:24 +0800

    chat

--- src/main/java/com/yumi/WeChatServer/util/AesUtils.java
+++ src/main/java/com/yumi/WeChatServer/util/AesUtils.java
@@ -7,18 +7,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AesUtils {
-
-    private static ApplicationContext applicationContext;
-
-    /*
-     * 这里我直接使用的spring来读取weiInfo对象中的加解密密钥的内容，大家可以不用这样，直接把密钥放在这里就可以了，
-     * 也可以放在properties文件中读取更方便
-     */
-
     public static String descMessage(String encrypt, String msgSignature,
                                      String timestamp, String nonce, WeiXinInfo weiXinInfo) throws AesException {
-        applicationContext = new ClassPathXmlApplicationContext(
-                "config/application-context.xml");
         String token = weiXinInfo.getToken();
         String encodingAesKey = weiXinInfo.getEncodingAESKey();
         String appId = weiXinInfo.getAppId();
@@ -33,8 +23,6 @@ public class AesUtils {
     /*加密和解密类似，就不做注释了*/
     public static String aescMessage(String replyMsg, String timestamp,
                                      String nonce, WeiXinInfo weiXinInfo) throws AesException {
-        applicationContext = new ClassPathXmlApplicationContext(
-                "config/application-context.xml");
         String token = weiXinInfo.getToken();
         String encodingAesKey = weiXinInfo.getEncodingAESKey();
         String appId = weiXinInfo.getAppId();
